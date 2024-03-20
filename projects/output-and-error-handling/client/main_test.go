002bee6c3d2c97842a70f3230fe9d28621c4fe0c
SaadiaELF <saadiaelfekak@gmail.com>
2024-03-20 20:42:39 +0000

    Add 2 success cases

--- projects/output-and-error-handling/client/main_test.go
+++ projects/output-and-error-handling/client/main_test.go
@@ -1,28 +1,34 @@
 package main
 
 import (
+	"fmt"
 	"io"
 	"net/http"
 	"net/http/httptest"
 	"testing"
 )
 
-func TestGetWeather(t *testing.T) {
-	expected := []byte("Today it will be sunny!")
-	svr := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		w.WriteHeader(http.StatusOK)
-		w.Write(expected)
-	}))
-	defer svr.Close()
-	client := &http.Client{}
-	resp, err := getWeather(svr.URL, client)
-	if err != nil {
-		t.Errorf("expected err to be nil got %v", err)
-	}
-	defer resp.Body.Close()
-	body, _ := io.ReadAll(resp.Body)
+func TestGetWeather_Success(t *testing.T) {
+	expRespSlice := [][]byte{[]byte("Today it will be sunny!"), []byte("Tomorrow it will be rainy!")}
 
-	if string(body) != string(expected) {
-		t.Errorf("expected res to be %s got %s", expected, body)
+	for i, expected := range expRespSlice {
+		t.Run(fmt.Sprintf("Case %d", i+1), func(t *testing.T) {
+			svr := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+				w.WriteHeader(http.StatusOK)
+				w.Write(expected)
+			}))
+			defer svr.Close()
+			client := &http.Client{}
+			resp, err := getWeather(svr.URL, client)
+			if err != nil {
+				t.Errorf("expected err to be nil got %v", err)
+			}
+			defer resp.Body.Close()
+			body, _ := io.ReadAll(resp.Body)
+
+			if string(body) != string(expected) {
+				t.Errorf("expected response body to be %s got %s", expected, body)
+			}
+		})
 	}
 }
