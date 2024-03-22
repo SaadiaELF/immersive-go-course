d0bb549ed0ea7fa7b7b395806295d61bcc745e4d
SaadiaELF <saadiaelfekak@gmail.com>
2024-03-22 23:58:13 +0000

    Add path

--- projects/cli-files/go-ls/cmd/root.go
+++ projects/cli-files/go-ls/cmd/root.go
@@ -6,8 +6,12 @@ import (
 )
 
 func Execute() {
+	path := "."
+	if len(os.Args) > 1  {
+		path = os.Args[1]
+	}
 	// Open the current directory
-	dir, err := os.Open(".")
+	dir, err := os.Open(path)
 	if err != nil {
 		fmt.Printf("Error opening directory: %v\n", err)
 		return
