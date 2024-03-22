1a36fd0b821308eda5c6e5fab9b6701abf8af758
SaadiaELF <saadiaelfekak@gmail.com>
2024-03-22 11:41:30 +0000

    Add converTime test

--- projects/output-and-error-handling/client/main_test.go
+++ projects/output-and-error-handling/client/main_test.go
@@ -4,7 +4,6 @@ import (
 	"fmt"
 	"net/http"
 	"net/http/httptest"
-	"strconv"
 	"testing"
 	"time"
 )
@@ -20,7 +19,7 @@ func TestHandleWeatherRequest_Success(t *testing.T) {
 			}))
 			defer svr.Close()
 			client := &http.Client{}
-			resp, err := handleWeatherRequest(svr.URL, client, time.Now(), 3)
+			resp, err := makeWeatherRequest(svr.URL, client, 3)
 			if err != nil {
 				t.Errorf("expected err to be nil got %v", err)
 			}
@@ -42,7 +41,7 @@ func TestHandleWeatherRequest_InternalServerError(t *testing.T) {
 	defer svr.Close()
 
 	client := &http.Client{}
-	_, err := handleWeatherRequest(svr.URL, client, time.Now(), 3)
+	_, err := makeWeatherRequest(svr.URL, client, 3)
 	if err == nil {
 		t.Error("expected error, got nil")
 	}
@@ -53,32 +52,73 @@ func TestHandleWeatherRequest_InternalServerError(t *testing.T) {
 	}
 }
 
-func TestHandleRateLimited(t *testing.T) {
-	testCases := []int{3, 6, -1}
+type MockTimeProvider struct{}
+
+func (m MockTimeProvider) Now() time.Time {
+	return time.Date(2024, time.March, 22, 12, 0, 0, 0, time.UTC)
+}
+
+func (m MockTimeProvider) Until(t time.Time) time.Duration {
+	return t.Sub(m.Now())
+}
+
+func TestConvertTime(t *testing.T) {
+	tp := MockTimeProvider{}
+	type testCase struct {
+		retryAfterTime string
+		expectedValue  int
+		expectedError  error
+	}
+
+	testCases := []testCase{
+		{
+			retryAfterTime: "3",
+			expectedValue:  3,
+			expectedError:  nil,
+		},
+		{
+			retryAfterTime: "6",
+			expectedValue:  6,
+			expectedError:  nil,
+		},
+		{
+			retryAfterTime: (tp.Now().Add(3 * time.Second)).Format(http.TimeFormat),
+			expectedValue:  3,
+			expectedError:  nil,
+		},
+		{
+			retryAfterTime: (tp.Now().Add(6 * time.Second)).Format(http.TimeFormat),
+			expectedValue:  6,
+			expectedError:  nil,
+		},
+		{
+			retryAfterTime: "a while",
+			expectedValue:  5,
+			expectedError:  nil,
+		},
+		{
+			retryAfterTime: "Invalid Format",
+			expectedValue:  0,
+			expectedError:  fmt.Errorf("internal Error: Failed to convert retry time"),
+		},
+	}
 	for i, tc := range testCases {
 		t.Run(fmt.Sprintf("Case %d", i+1), func(t *testing.T) {
-			retryAfterHeader := strconv.Itoa(tc)
-			svr := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-				w.Header().Set("Retry-After", retryAfterHeader)
-			}))
-			defer svr.Close()
 
-			err := handleRateLimited(retryAfterHeader, time.Now(), 2)
-			if tc == 3 {
+			seconds, err := convertTime(tc.retryAfterTime, tp)
+			if tc.expectedError == nil {
+				if seconds != tc.expectedValue {
+					t.Errorf("expected seconds to equal to %d got %d", seconds, tc.expectedValue)
+				}
 				if err != nil {
 					t.Errorf("expected err to be nil got %v", err)
 				}
-			}
-			if tc == 6 || tc == -1 {
-				if err == nil {
-					t.Error("expected error, got nil")
+			} else {
+				if err.Error() != tc.expectedError.Error() {
+					t.Errorf("expected error message to be %s, got %s", tc.expectedError, err.Error())
 				}
+			}
 
-				expectedError := "internal Error : Failed to retry"
-				if err.Error() != expectedError {
-					t.Errorf("expected error message to be %s, got %s", expectedError, err.Error())
-				}
-			}
 		})
 	}
 }
