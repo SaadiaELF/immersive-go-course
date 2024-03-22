4a77eb536298591df7b969e02aa2c9292f4b4b88
SaadiaELF <saadiaelfekak@gmail.com>
2024-03-22 23:42:30 +0000

    Add main functionality

--- projects/cli-files/go-ls/cmd/root.go
+++ projects/cli-files/go-ls/cmd/root.go
@@ -1,3 +1,26 @@
 package cmd
 
-func Execute() {}
+import (
+	"fmt"
+	"os"
+)
+
+func Execute() {
+	// Open the current directory
+	dir, err := os.Open(".")
+	if err != nil {
+		fmt.Printf("Error opening directory: %v\n", err)
+		return
+	}
+	defer dir.Close()
+
+	// Read directory all entries
+	entries, err := dir.ReadDir(0)
+	if err != nil {
+		fmt.Printf("Error reading directory: %v\n", err)
+		return
+	}
+	for _, entry := range entries {
+		fmt.Println(entry.Name())
+	}
+}
