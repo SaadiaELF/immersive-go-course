05b12f1c480a6d9a4a2de7459e26ca3064dcb703
SaadiaELF <saadiaelfekak@gmail.com>
2024-03-23 17:59:48 +0000

    Add -h flag

--- projects/cli-files/go-ls/cmd/root.go
+++ projects/cli-files/go-ls/cmd/root.go
@@ -1,32 +1,53 @@
 package cmd
 
 import (
+	"flag"
 	"fmt"
 	"os"
 )
 
-func Execute() {
-	var arg string
-	if len(os.Args) > 1 {
-		arg = os.Args[1]
-	} else {
-		arg, _ = os.Getwd()
-	}
-	if arg == "-h" {
-		fmt.Println("go-ls : list directory contents")
-	} else {
-		entries, err := os.ReadDir(arg)
-		if err != nil {
-			fdopendir := fmt.Sprintf("fdopendir %s: not a directory", arg)
-			if err.Error() == fdopendir {
-				fmt.Println(arg)
-			} else {
-				fmt.Println(err)
-			}
-			return
-		}
-		for _, entry := range entries {
-			fmt.Println(entry.Name())
-		}
+func boldText(s string) string {
+	return fmt.Sprintf("\033[1m%s\033[0m", s)
+}
+func init() {
+	flag.Usage = func() {
+		fmt.Printf("%s\n%s\n	go-ls - list directory content\n%s\n	go-ls [path] || go-ls [options]\n", boldText("Usage of go-ls"), boldText("Name:"), boldText("Format:"))
+		fmt.Printf("%s\n	For each operand that names a file of a type other than directory, go-ls displays its name.\n	For each operand that names a file of type directory, go-ls displays the names of files contained within that directory.\n	If no operands are given, the contents of the current directory are displayed.\n", boldText("Description:"))
+		fmt.Println("The following options are available:")
+		flag.PrintDefaults()
 	}
 }
+
+func Execute() {
+	var path string
+
+	h := flag.Bool("h", false, "show description and usage")
+	flag.Parse()
+
+	if *h {
+		flag.Usage()
+		return
+	}
+
+	if len(os.Args) > 1 {
+		path = os.Args[1]
+	} else {
+		path, _ = os.Getwd()
+	}
+
+	entries, err := os.ReadDir(path)
+	if err != nil {
+		fdopendir := fmt.Sprintf("fdopendir %s: not a directory", path)
+		if err.Error() == fdopendir {
+			fmt.Println(path)
+		} else {
+			fmt.Println(err)
+		}
+		return
+	}
+	for _, entry := range entries {
+		fmt.Println(entry.Name())
+	}
+}
+
+// }
