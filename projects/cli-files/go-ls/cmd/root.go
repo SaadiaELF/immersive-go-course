bc3a9aac3964a85148995c34b059eafc085773f0
SaadiaELF <saadiaelfekak@gmail.com>
2024-03-23 00:43:32 +0000

    Refactroing

--- projects/cli-files/go-ls/cmd/root.go
+++ projects/cli-files/go-ls/cmd/root.go
@@ -10,10 +10,15 @@ func Execute() {
 	if len(os.Args) > 1 {
 		path = os.Args[1]
 	}
-	
+
 	entries, err := os.ReadDir(path)
 	if err != nil {
-		fmt.Println(path)
+		fdopendir := fmt.Sprintf("fdopendir %s: not a directory", path)
+		if err.Error() == fdopendir {
+			fmt.Println(path)
+		} else {
+			fmt.Println(err)
+		}
 		return
 	}
 	for _, entry := range entries {
