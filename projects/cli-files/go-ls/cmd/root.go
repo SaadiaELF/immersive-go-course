ef3a3516c115df411316ebc93350deded5ddda15
SaadiaELF <saadiaelfekak@gmail.com>
2024-03-23 00:09:17 +0000

    Use os.ReadDir and delete Opening file

--- projects/cli-files/go-ls/cmd/root.go
+++ projects/cli-files/go-ls/cmd/root.go
@@ -7,19 +7,11 @@ import (
 
 func Execute() {
 	path := "."
-	if len(os.Args) > 1  {
+	if len(os.Args) > 1 {
 		path = os.Args[1]
 	}
-	// Open the current directory
-	dir, err := os.Open(path)
-	if err != nil {
-		fmt.Printf("Error opening directory: %v\n", err)
-		return
-	}
-	defer dir.Close()
-
 	// Read directory all entries
-	entries, err := dir.ReadDir(0)
+	entries, err := os.ReadDir(path)
 	if err != nil {
 		fmt.Printf("Error reading directory: %v\n", err)
 		return
