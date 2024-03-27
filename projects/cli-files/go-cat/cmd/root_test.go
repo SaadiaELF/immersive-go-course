490466ffca0ab6f197d5bb5183abf9504fb8d468
SaadiaELF <saadiaelfekak@gmail.com>
2024-03-27 03:29:41 +0000

    Refactor test cases

--- projects/cli-files/go-cat/cmd/root_test.go
+++ projects/cli-files/go-cat/cmd/root_test.go
@@ -8,15 +8,10 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-// cat [file-path] [file-path] displays the content of both files
-// Extra test cases:
-// cat [file-path] [directory-path] displays error message : [directory-path] is a directory
-// cat [file-path] [non-existent-file-path] displays error message : [non-existent-file-path]: no such file or directory
-// cat [directory-path] [file-path] displays error message : [directory-path] is a directory
 
 // cat [non-existent-file-path] displays error message : [non-existent-file-path]: no such file or directory
 func TestNoFileSpecified(t *testing.T) {
-	err := checkArgs([]string{})
+	err := checkArgs("")
 	require.Equal(t, err.Error(), "error: no file specified")
 }
 
@@ -24,20 +19,20 @@ func TestNoFileSpecified(t *testing.T) {
 func TestDirectoryPath(t *testing.T) {
 	testCases := []struct {
 		name string
-		args []string
-	}{{name: "current directory", args: []string{"."}}, {name: "directory path", args: []string{"../../assets"}}}
+		arg  string
+	}{{name: "current directory", arg: "."}, {name: "directory path", arg: "../../assets"}}
 	for _, tc := range testCases {
 		t.Run(tc.name, func(t *testing.T) {
-			err := checkArgs(tc.args)
-			require.Equal(t, "error: '"+tc.args[0]+"': is a directory", err.Error())
+			err := checkArgs(tc.arg)
+			require.Equal(t, "error: '"+tc.arg+"': is a directory", err.Error())
 		})
 	}
 }
 
 // cat [non-existent-file-path] displays error message : [non-existent-file-path]: no such file or directory
 func TestNonExistentFilePath(t *testing.T) {
-	err := checkArgs([]string{"non-existent-file-path"})
-	require.Equal(t, "error: 'non-existent-file-path': no such file or directory", err.Error())
+	err := checkArgs("/non-existent-file-path")
+	require.Equal(t, "error: '/non-existent-file-path': no such file or directory", err.Error())
 }
 
 func createTempFile() string {
@@ -55,8 +50,10 @@ func createTempFile() string {
 // cat [file-path] displays the content of a file
 func TestValidFilePath(t *testing.T) {
 	fileName := createTempFile()
-	fileLines, err := readFile(fileName)
+	fileLines, err := readFileLines(fileName)
 	require.NoError(t, err)
 	require.Equal(t, []string{"file contents"}, fileLines)
 	defer os.Remove(fileName)
 }
+
+// cat [file-path] [file-path] displays the content of both files
