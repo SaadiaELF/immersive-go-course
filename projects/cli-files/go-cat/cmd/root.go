015b3fbcff1e1e34034e4070e0f4bdd7caca2a77
SaadiaELF <saadiaelfekak@gmail.com>
2024-03-27 03:12:32 +0000

    Add file reading functionality to go-cat command

--- projects/cli-files/go-cat/cmd/root.go
+++ projects/cli-files/go-cat/cmd/root.go
@@ -1,7 +1,9 @@
 package cmd
 
 import (
+	"bufio"
 	"fmt"
+	"io"
 	"os"
 )
 
@@ -11,6 +13,13 @@ func Execute() {
 	if err != nil {
 		fmt.Fprintln(os.Stderr, err)
 		return
+	}
+	fileLines, err := readFile(args[0])
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+	}
+	for _, fileLine := range fileLines {
+		fmt.Print(fileLine)
 	}
 }
 
@@ -28,4 +37,27 @@ func checkArgs(args []string) error {
 		}
 	}
 	return nil
+}
+
+func readFile(filePath string) ([]string, error) {
+	file, err := os.Open(filePath)
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+
+	reader := bufio.NewReader(file)
+	buffer := make([]byte, 16)
+	fileLines := []string{}
+	for {
+		n, err := reader.Read(buffer)
+		if err != nil {
+			if err != io.EOF {
+				return nil, err
+			}
+			break
+		}
+		fileLines = append(fileLines, string(buffer[0:n]))
+	}
+	return fileLines, nil
 }
