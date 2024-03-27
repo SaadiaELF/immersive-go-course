9cccc4926e42347a80250918f2268c1941ee65a5
SaadiaELF <saadiaelfekak@gmail.com>
2024-03-27 01:34:49 +0000

    handle directory case

--- projects/cli-files/go-cat/cmd/root.go
+++ projects/cli-files/go-cat/cmd/root.go
@@ -1,13 +1,24 @@
 package cmd
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+)
 
 func Execute() {
-	args := checkArgs([]string{})
-	fmt.Println(args)
+	args := os.Args[1:]
+	checkArgs(args)
+	fmt.Println(checkArgs(args))
 }
 
 func checkArgs(args []string) (err error) {
+
+	if len(args) == 1 {
+		fileInfo, _ := os.Stat(args[0])
+		if fileInfo.IsDir() {
+			err = fmt.Errorf("error: '%s': is a directory", args[0])
+		}
+	}
 	if len(args) == 0 {
 		err = fmt.Errorf("error: no file specified")
 	}
