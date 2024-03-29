2efe6dd180e2b9ec173db0252d41b014e2f49c44
SaadiaELF <saadiaelfekak@gmail.com>
2024-03-29 02:17:21 +0000

    Add OurByteBuffer struct and methods

--- projects/interfaces/main.go
+++ projects/interfaces/main.go
@@ -1,5 +1,17 @@
 package main
 
+type OurByteBuffer struct {
+	bytes []byte
+}
+
+func NewBuffer(b []byte) *OurByteBuffer {
+	return &OurByteBuffer{bytes: b}
+}
+
+func (b *OurByteBuffer) Bytes() []byte {
+	return b.bytes
+}
+
 func main() {
 
 }
