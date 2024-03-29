0db1d8eb091881a19f334eb624daef75a12cb15d
SaadiaELF <saadiaelfekak@gmail.com>
2024-03-29 01:49:20 +0000

    Refactor test function names

--- projects/interfaces/main_test.go
+++ projects/interfaces/main_test.go
@@ -7,11 +7,10 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-// If you call b.Read() with a slice big enough to read all of the bytes in the buffer, all of the bytes are read.
 // If you call b.Read() with a slice smaller than the contents of the buffer, some of the bytes are read. If you call it again, the next bytes are read.
 
 // If you make a buffer named b containing some bytes, calling b.Bytes() returns the same bytes you created it with.
-func TestBytesMethod(t *testing.T) {
+func TestBufferBytesReturnsInitialBytes(t *testing.T) {
 	inputBytes := []byte("Hello, World!")
 	b := bytes.NewBuffer(inputBytes)
 
@@ -21,7 +20,7 @@ func TestBytesMethod(t *testing.T) {
 }
 
 // If you write some extra bytes to that buffer using b.Write(), a call to b.Bytes() returns both the initial bytes and the extra bytes.
-func TestWriteMethod(t *testing.T) {
+func TestBufferWriteExtraBytes(t *testing.T) {
 	inputBytes := []byte("Hello ")
 	b := bytes.NewBuffer(inputBytes)
 	n, _ := b.Write([]byte("World!"))
