ac9f098859a020aeafefded72643e599fbb1b70e
SaadiaELF <saadiaelfekak@gmail.com>
2024-03-29 01:45:37 +0000

    Add a new test for the Write method

--- projects/interfaces/main_test.go
+++ projects/interfaces/main_test.go
@@ -7,7 +7,6 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-// If you write some extra bytes to that buffer using b.Write(), a call to b.Bytes() returns both the initial bytes and the extra bytes.
 // If you call b.Read() with a slice big enough to read all of the bytes in the buffer, all of the bytes are read.
 // If you call b.Read() with a slice smaller than the contents of the buffer, some of the bytes are read. If you call it again, the next bytes are read.
 
@@ -19,4 +18,15 @@ func TestBytesMethod(t *testing.T) {
 	bytes := b.Bytes()
 
 	require.Equal(t, len(inputBytes), len(bytes))
+}
+
+// If you write some extra bytes to that buffer using b.Write(), a call to b.Bytes() returns both the initial bytes and the extra bytes.
+func TestWriteMethod(t *testing.T) {
+	inputBytes := []byte("Hello ")
+	b := bytes.NewBuffer(inputBytes)
+	n, _ := b.Write([]byte("World!"))
+
+	bytes := b.Bytes()
+
+	require.Equal(t, len(inputBytes)+n, len(bytes))
 }
