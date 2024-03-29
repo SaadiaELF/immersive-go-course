6f4a19d89c1dda69801751b35b5ac6d52d8d4ea9
SaadiaELF <saadiaelfekak@gmail.com>
2024-03-29 16:52:23 +0000

    update test cases

--- projects/interfaces/buffer_test.go
+++ projects/interfaces/buffer_test.go
@@ -1,6 +1,7 @@
 package OurBuffer
 
 import (
+	"io"
 	"testing"
 
 	"github.com/stretchr/testify/require"
@@ -13,18 +14,24 @@ func TestBufferBytesReturnsInitialBytes(t *testing.T) {
 
 	bytes := b.Bytes()
 
-	require.Equal(t, len(inputBytes), len(bytes))
+	excepted := string(inputBytes)
+	actual := string(bytes)
+
+	require.Equal(t, excepted, actual)
 }
 
 // If you write some extra bytes to that buffer using b.Write(), a call to b.Bytes() returns both the initial bytes and the extra bytes.
 func TestBufferWriteExtraBytes(t *testing.T) {
 	inputBytes := []byte("Hello ")
 	b := NewBuffer(inputBytes)
-	n, _ := b.Write([]byte("World!"))
+	n, err := b.Write([]byte("World!"))
+	require.NoError(t, err)
 
 	bytes := b.Bytes()
-
-	require.Equal(t, len(inputBytes)+n, len(bytes))
+	expected := string((bytes))
+	actual := string(inputBytes) + "World!"
+	require.Equal(t, expected, actual)
+	require.Equal(t, len(bytes), len(inputBytes)+n)
 }
 
 // If you call b.Read() with a slice big enough to read all of the bytes in the buffer, all of the bytes are read.
@@ -33,8 +40,12 @@ func TestBufferReadFullSlice(t *testing.T) {
 	b := NewBuffer(inputBytes)
 
 	outputBytes := make([]byte, len(inputBytes))
-	n, _ := b.Read(outputBytes)
+	n, err := b.Read(outputBytes)
+	require.ErrorAs(t, err, &io.EOF)
 
+	expected := string(inputBytes)
+	actual := string(outputBytes)
+	require.Equal(t, expected, actual)
 	require.Equal(t, len(inputBytes), n)
 }
 
@@ -44,8 +55,19 @@ func TestBufferReadPartialSlices(t *testing.T) {
 	b := NewBuffer(inputBytes)
 
 	outputBytes := make([]byte, 7)
-	b.Read(outputBytes)
-	require.Equal(t, b.String(), "World!")
-	b.Read(outputBytes)
-	require.Equal(t, b.String(), "")
+	t.Run("first read", func(t *testing.T) {
+		n, err := b.Read(outputBytes)
+		require.NoError(t, err)
+		require.Equal(t, "World!", b.String())
+		require.Equal(t, "Hello, ", string(outputBytes))
+		require.Equal(t, 7, n)
+	})
+
+	t.Run("Second Read", func(t *testing.T) {
+		n, err := b.Read(outputBytes)
+		require.ErrorAs(t, err, &io.EOF)
+		require.Equal(t, b.String(), "")
+		require.Equal(t, "World! ", string(outputBytes))
+		require.Equal(t, 6, n)
+	})
 }
