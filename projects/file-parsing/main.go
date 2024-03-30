9ed4e1c30ddfaa2b30e84ab77688fd3471b84ad7
SaadiaELF <saadiaelfekak@gmail.com>
2024-03-30 03:51:09 +0000

    Add function to find player with highest score

--- projects/file-parsing/main.go
+++ projects/file-parsing/main.go
@@ -7,11 +7,24 @@ import (
 	jsonparser "github.com/CodeYourFuture/immersive-go-course/projects/file-parsing/fileparser"
 )
 
+func getHighScorePlayer(players jsonparser.Players) string {
+	highScore := 0
+	highScorePlayer := ""
+	for _, player := range players {
+		if player.HighScore > highScore {
+			highScore = player.HighScore
+			highScorePlayer = player.Name
+		}
+	}
+	return highScorePlayer
+}
+
 func main() {
 	data, err := jsonparser.ParseJSON("./examples/json.txt")
 	if err != nil {
 		fmt.Fprintln(os.Stderr, err)
 		os.Exit(1)
 	}
-	fmt.Println(data)
+	highScorePlayer := getHighScorePlayer(data)
+	fmt.Printf("The player with the highest score is '%s'\n", highScorePlayer)
 }
