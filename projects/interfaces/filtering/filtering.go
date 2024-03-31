e33676ee4edf5c155d4650af0b3bb1d7f43bc766
SaadiaELF <saadiaelfekak@gmail.com>
2024-03-31 04:44:48 +0100

    Fix filteringPipe to remove non-numeric characters

--- projects/interfaces/filtering/filtering.go
+++ projects/interfaces/filtering/filtering.go
@@ -11,15 +11,18 @@ type filteringPipe struct {
 func NewFilteringPipe(w io.Writer) io.Writer {
 	return filteringPipe{fw: w}
 }
+func remove(slice []byte, s int) []byte {
+	copy(slice[s:], slice[s+1:])
+	return slice[:len(slice)-1]
+}
 
 func (f filteringPipe) Write(p []byte) (n int, err error) {
-	filtered := []byte{}
-	for _, value := range p {
+	for i, value := range p {
 		if value < '0' || value > '9' {
-			filtered = append(filtered, value)
+			remove(p, i)
 		}
 	}
-	n, err = f.fw.Write(filtered)
+	n, err = f.fw.Write(p)
 	if err != nil {
 		return n, err
 	}
