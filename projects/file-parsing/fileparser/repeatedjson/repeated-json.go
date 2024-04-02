96f9cea3542e3961fdc0f4444a96321a6c9cfc1a
SaadiaELF <saadiaelfekak@gmail.com>
2024-04-02 13:46:34 +0100

    Add repeated JSON parser function

--- projects/file-parsing/fileparser/repeatedjson/repeated-json.go
+++ projects/file-parsing/fileparser/repeatedjson/repeated-json.go
@@ -1,1 +1,39 @@
 package repeatedjson
+
+import (
+	"bufio"
+	"encoding/json"
+	"os"
+	"strings"
+)
+
+type Player struct {
+	Name      string `json:"name"`
+	HighScore int    `json:"high_score"`
+}
+
+type Players []Player
+
+func RepeatedJSONParser(filename string) (players Players, err error) {
+	file, err := os.Open(filename)
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+
+	for scanner.Scan() {
+		line := scanner.Text()
+		if strings.HasPrefix(line, "#") {
+			continue
+		}
+		player := Player{}
+		err = json.Unmarshal([]byte(line), &player)
+		if err != nil {
+			return nil, err
+		}
+		players = append(players, player)
+	}
+	return players, nil
+}
