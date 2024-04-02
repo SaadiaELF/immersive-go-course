440851510281ce31ecc19d9f4f759070f6826b59
SaadiaELF <saadiaelfekak@gmail.com>
2024-04-02 10:56:53 +0100

    Add test for getLowestScorePlayer

--- projects/file-parsing/main_test.go
+++ projects/file-parsing/main_test.go
@@ -7,14 +7,21 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-func TestGetHighScorePlayer(t *testing.T) {
-	players := jsonparser.Players{
-		{Name: "Aya", HighScore: 10},
-		{Name: "Prisha", HighScore: 30},
-		{Name: "Charlie", HighScore: -1},
-		{Name: "Margot", HighScore: 25},
-	}
-	actual := getHighScorePlayer(players)
+var players = jsonparser.Players{
+	{Name: "Aya", HighScore: 10},
+	{Name: "Prisha", HighScore: 30},
+	{Name: "Charlie", HighScore: -1},
+	{Name: "Margot", HighScore: 25},
+}
+
+func TestGetHighestScorePlayer(t *testing.T) {
+	actual := getHighestScorePlayer(players)
 	expected := "Prisha"
+	require.Equal(t, expected, actual)
+}
+
+func TestGetLowestScorePlayer(t *testing.T) {
+	actual := getLowestScorePlayer(players)
+	expected := "Charlie"
 	require.Equal(t, expected, actual)
 }
