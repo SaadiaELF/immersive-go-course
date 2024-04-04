17e1cb7ce1677376fc440bd3b75d2fe81d0593fd
SaadiaELF <saadiaelfekak@gmail.com>
2024-04-04 03:02:08 +0100

    Add test cases for BinaryParser function

--- projects/file-parsing/fileparser/binary/binary_test.go
+++ projects/file-parsing/fileparser/binary/binary_test.go
@@ -25,3 +25,21 @@ func TestByteOrder(t *testing.T) {
 		})
 	}
 }
+
+func TestBinaryParser(t *testing.T) {
+	players := Players{{Name: "Aya", HighScore: 10}, {Name: "Prisha", HighScore: 30}, {Name: "Charlie", HighScore: -1}, {Name: "Margot", HighScore: 25}}
+	testCases := []struct {
+		filename string
+		expected Players
+	}{
+		{filename: "../../examples/custom-binary-le.bin", expected: players},
+		{filename: "../../examples/custom-binary-be.bin", expected: players},
+	}
+	for _, tc := range testCases {
+		t.Run(tc.filename, func(t *testing.T) {
+			players, err := BinaryParser(tc.filename)
+			require.NoError(t, err)
+			require.Equal(t, tc.expected, players)
+		})
+	}
+}
