a3406061c3965442509b5927154437baa54f888f
SaadiaELF <saadiaelfekak@gmail.com>
2024-04-04 23:00:10 +0100

    Refactor file parsing logic and add support for multiple parsers

--- projects/file-parsing/main.go
+++ projects/file-parsing/main.go
@@ -4,41 +4,56 @@ import (
 	"fmt"
 	"os"
 
-	jsonparser "github.com/CodeYourFuture/immersive-go-course/projects/file-parsing/fileparser/json"
+	"github.com/CodeYourFuture/immersive-go-course/projects/file-parsing/parsers/binary"
+	"github.com/CodeYourFuture/immersive-go-course/projects/file-parsing/parsers/jsonparser"
+	"github.com/CodeYourFuture/immersive-go-course/projects/file-parsing/parsers/repeatedjsonparser"
+	"github.com/CodeYourFuture/immersive-go-course/projects/file-parsing/types"
+	"github.com/CodeYourFuture/immersive-go-course/projects/file-parsing/utils"
 )
 
-func getHighestScorePlayer(players jsonparser.Players) string {
-	highestScore := players[0].HighScore
-	highestScorePlayer := ""
-	for _, player := range players {
-		if player.HighScore >= highestScore {
-			highestScore = player.HighScore
-			highestScorePlayer = player.Name
-		}
-	}
-	return highestScorePlayer
-}
-
-func getLowestScorePlayer(players jsonparser.Players) string {
-	lowestScore := players[0].HighScore
-	lowestScorePlayer := ""
-	for _, player := range players {
-		if player.HighScore <= lowestScore {
-			lowestScore = player.HighScore
-			lowestScorePlayer = player.Name
-		}
-	}
-	return lowestScorePlayer
-}
-
 func main() {
-	data, err := jsonparser.JSONParser("./examples/json.txt")
+
+	// open a directory and read all files
+	// for each file, parse it and get the highest and lowest score player
+	// print the highest and lowest score player
+	dataMap := make(map[string]types.Players)
+	dir := "./examples"
+	files, err := os.ReadDir(dir)
 	if err != nil {
 		fmt.Fprintln(os.Stderr, err)
 		os.Exit(1)
 	}
-	highestScorePlayer := getHighestScorePlayer(data)
-	lowestScorePlayer := getLowestScorePlayer(data)
-	fmt.Printf("The player with the highest score is '%s'\n", highestScorePlayer)
-	fmt.Printf("The player with the lowest score is '%s'\n", lowestScorePlayer)
+	var myParser types.Parser
+	var data types.Players
+	for _, file := range files {
+		filepath := dir + "/" + file.Name()
+
+		switch filepath {
+		case "./examples/json.txt":
+			myParser = jsonparser.Parser{}
+		case "./examples/repeated-json.txt":
+			myParser = repeatedjsonparser.Parser{}
+		case "./examples/custom-binary-be.bin":
+			myParser = binary.Parser{}
+		case "./examples/custom-binary-le.bin":
+			myParser = binary.Parser{}
+		default:
+			fmt.Fprintf(os.Stderr, "Unknown file type: %s\n", filepath)
+		}
+		data, err = myParser.Parse(filepath)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "Error parsing file %s: %v\n", filepath, err)
+			continue // Skip to the next file if parsing fails
+		}
+
+		dataMap[filepath] = data
+	}
+
+	for filepath, data := range dataMap {
+		highestScorePlayer := utils.GetHighestScorePlayer(data)
+		lowestScorePlayer := utils.GetLowestScorePlayer(data)
+
+		fmt.Printf("The player with the highest score in %s is %s with %d\n", filepath, highestScorePlayer.Name, highestScorePlayer.HighScore)
+		fmt.Printf("The player with the lowest score in %s is %s with %d\n", filepath, lowestScorePlayer.Name, lowestScorePlayer.HighScore)
+	}
 }
