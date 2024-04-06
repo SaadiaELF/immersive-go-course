ccfe3becfa3a51bb6ee0b19e4c39452ca63dbc4c
SaadiaELF <saadiaelfekak@gmail.com>
2024-04-06 12:40:57 +0100

    Update TestPut test cases

--- projects/concurrency/main_test.go
+++ projects/concurrency/main_test.go
@@ -6,27 +6,26 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-func (c *Cache) GetKeys() []int {
-	keys := make([]int, 0, len(c.storage))
-	for key := range c.storage {
-		keys = append(keys, key)
-	}
-	return keys
-}
 func TestPut(t *testing.T) {
 	cache := NewCache(2)
 	t.Run("Case1/Below capacity", func(t *testing.T) {
 		cache.Put(1, "one")
 		cache.Put(2, "two")
-		actual := cache.GetKeys()
-		require.Equal(t, []int{1, 2}, actual)
+		_, exists := cache.storage[1]
+		require.True(t, exists)
+		_, exists = cache.storage[2]
+		require.True(t, exists)
 	})
 	t.Run("Case1/Beyond capacity", func(t *testing.T) {
 		cache.Put(1, "one")
 		cache.Put(2, "two")
 		cache.Put(3, "three")
-		actual := cache.GetKeys()
-		require.Equal(t, []int{2, 3}, actual)
+		_, exists := cache.storage[1]
+		require.False(t, exists)
+		_, exists = cache.storage[2]
+		require.True(t, exists)
+		_, exists = cache.storage[3]
+		require.True(t, exists)
 	})
 }
 
