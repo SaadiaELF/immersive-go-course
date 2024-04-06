8ddf88272150f559f507fdb9815fb12bd09fa79d
SaadiaELF <saadiaelfekak@gmail.com>
2024-04-06 04:49:42 +0100

    Add tests for Get function

--- projects/concurrency/main_test.go
+++ projects/concurrency/main_test.go
@@ -29,3 +29,21 @@ func TestPut(t *testing.T) {
 		require.Equal(t, []int{2, 3}, actual)
 	})
 }
+
+func TestGet(t *testing.T) {
+	cache := NewCache(2)
+	cache.Put(1, "one")
+	cache.Put(2, "two")
+	t.Run("Case1/Exists", func(t *testing.T) {
+		value, exists := cache.Get(1)
+		require.True(t, exists)
+		require.Equal(t, "one", value)
+		// check the most recently used key is 1
+		require.True(t, cache.storage[1].lastUsed.After(cache.storage[2].lastUsed))
+	})
+	t.Run("Case2/Does not exist", func(t *testing.T) {
+		value, exists := cache.Get(3)
+		require.False(t, exists)
+		require.Nil(t, value)
+	})
+}
