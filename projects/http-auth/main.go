ef87a9c249317566bf45441d205bd96414be2f55
SaadiaELF <saadiaelfekak@gmail.com>
2024-04-09 01:30:55 +0000

    Add HTTP status code handlers***

--- projects/http-auth/main.go
+++ projects/http-auth/main.go
@@ -8,8 +8,22 @@ import (
 
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("Hello, world"))
+		w.Write([]byte("Hello, world\n"))
 	})
+	// 200 status code
+	http.HandleFunc("/200", func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(200)
+		w.Write([]byte("200\n"))
+	})
+
+	// 500 status code
+	http.HandleFunc("/500", func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(500)
+		w.Write([]byte("Internal server error\n"))
+	})
+
+	// 404 status code
+	http.Handle("/404", http.NotFoundHandler())
 
 	fmt.Fprintln(os.Stderr, "Listening on port 8080...")
 	if err := http.ListenAndServe(":8080", nil); err != nil {
