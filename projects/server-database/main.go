5d65ad03a8049c02be58c22fd0d5bb32cdb89adc
SaadiaELF <saadiaelfekak@gmail.com>
2024-04-10 01:32:44 +0000

    Refactor fetchImages function to use a database connection

--- projects/server-database/main.go
+++ projects/server-database/main.go
@@ -15,7 +15,8 @@ import (
 )
 
 func handleImages(w http.ResponseWriter, r *http.Request) {
-	images, _ := fetchImages()
+	conn, _ := setDatabaseConnection()
+	images, _ := fetchImages(conn)
 	indent := getIndentParam(r)
 	b, err := json.MarshalIndent(images, "", indent)
 	if err != nil {
@@ -40,20 +41,20 @@ func getIndentParam(r *http.Request) string {
 	return indent
 }
 
-func fetchImages() ([]types.Image, error) {
-	images := []types.Image{
-		{
-			Title:   "Sunset",
-			AltText: "Clouds at sunset",
-			URL:     "https://images.unsplash.com/photo-1506815444479-bfdb1e96c566?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1000&q=80",
-		},
-		{
-			Title:   "Mountain",
-			AltText: "A mountain at sunset",
-			URL:     "https://images.unsplash.com/photo-1540979388789-6cee28a1cdc9?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1000&q=80",
-		},
+func fetchImages(conn *pgx.Conn) ([]types.Image, error) {
+	var images []types.Image
+	rows, err := conn.Query(context.Background(), "SELECT title, url, alt_text FROM public.images")
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error: failed to fetch images: %v", err)
 	}
-
+	for rows.Next() {
+		var title, url, altText string
+		err = rows.Scan(&title, &url, &altText)
+		images = append(images, types.Image{Title: title, URL: url, AltText: altText})
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "Error: failed to scan image: %v", err)
+		}
+	}
 	return images, nil
 }
 func setDatabaseConnection() (*pgx.Conn, error) {
@@ -75,7 +76,7 @@ func main() {
 	if err != nil {
 		log.Fatal("Error loading .env file")
 	}
-	setDatabaseConnection()
+
 	http.HandleFunc("/images.json", handleImages)
 
 	fmt.Fprintln(os.Stderr, "Listening on port 8080...")
