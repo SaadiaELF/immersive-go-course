e7c5f0150de2a2cb9b64447d7c812545938b0115
SaadiaELF <saadiaelfekak@gmail.com>
2024-04-11 21:51:56 +0000

    Add post image request

--- projects/server-database/main.go
+++ projects/server-database/main.go
@@ -73,31 +73,73 @@ func main() {
 	log.Println("Graceful shutdown complete.")
 }
 
+// handleImages fetches images from the database and returns them as a JSON response.
 func handleImages(w http.ResponseWriter, r *http.Request) {
 	if dbPool == nil {
 		w.WriteHeader(http.StatusInternalServerError)
 		fmt.Fprintln(os.Stderr, "Database connection is not set")
 	}
-	images, err := fetchImages(dbPool, 10)
-	if err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
-		fmt.Fprintf(os.Stderr, "Error: failed to fetch images: %v", err)
-	}
-	indent, err := getIndentParam(r)
-	if err != nil {
-		w.WriteHeader(http.StatusBadRequest)
-		fmt.Fprintf(os.Stderr, "Error: failed to parse indent: %v", err)
-	}
-	b, err := json.MarshalIndent(images, "", indent)
-	if err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
-		fmt.Fprintf(os.Stderr, "Error: failed to marshal images: %v", err)
-	}
 
-	w.Header().Set("Content-Type", "application/json")
-	w.Write(b)
+	if r.Method == "POST" {
+		postImage(w, r)
+	}
+	if r.Method == "GET" {
+		images, err := fetchImages(dbPool, 10)
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			fmt.Fprintf(os.Stderr, "Error: failed to fetch images: %v", err)
+		}
+
+		indent, err := getIndentParam(r)
+		if err != nil {
+			w.WriteHeader(http.StatusBadRequest)
+			fmt.Fprintf(os.Stderr, "Error: failed to parse indent: %v", err)
+		}
+
+		b, err := json.MarshalIndent(images, "", indent)
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			fmt.Fprintf(os.Stderr, "Error: failed to marshal images: %v", err)
+		}
+
+		w.Header().Set("Content-Type", "application/json")
+		w.Write(b)
+	}
 }
 
+// POST /images.json
+func postImage(w http.ResponseWriter, r *http.Request) {
+
+	// Parse the request body
+	var image types.Image
+	err := json.NewDecoder(r.Body).Decode(&image)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		fmt.Fprintf(os.Stderr, "Error: failed to decode image: %v", err)
+	}
+
+	// Check if the image url exists in the database
+	query := "SELECT url FROM public.images WHERE url = $1"
+	row := dbPool.QueryRow(context.Background(), query, image.URL)
+	var url string
+	err = row.Scan(&url)
+	if err == nil {
+		w.WriteHeader(http.StatusConflict)
+		fmt.Fprintf(os.Stderr, "Error: image with url %s already exists", image.URL)
+		return
+	}
+
+	// Insert the image into the database
+	query = "INSERT INTO public.images (title, url, alt_text) VALUES ($1, $2, $3)"
+	_, err = dbPool.Exec(context.Background(), query, image.Title, image.URL, image.AltText)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		fmt.Fprintf(os.Stderr, "Error: failed to insert image: %v", err)
+	}
+	w.WriteHeader(http.StatusCreated)
+}
+
+// fetchImages fetches images from the database.
 func fetchImages(pool *pgxpool.Pool, limit int) ([]types.Image, error) {
 	var images []types.Image
 	query := fmt.Sprintf("SELECT title, url, alt_text FROM public.images LIMIT %d", limit)
@@ -116,6 +158,7 @@ func fetchImages(pool *pgxpool.Pool, limit int) ([]types.Image, error) {
 	return images, err
 }
 
+// getIndentParam returns the indent parameter from the request query string.
 func getIndentParam(r *http.Request) (string, error) {
 	params := r.URL.Query()
 	indent := params.Get("indent")
