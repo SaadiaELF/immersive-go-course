89a738d6dcac57ba2c96d30945b0946f1f4fd117
SaadiaELF <saadiaelfekak@gmail.com>
2024-04-13 14:03:37 +0000

    Add command line flags and environment variable loading to main.go

--- projects/multiple-servers/cmd/api-server/main.go
+++ projects/multiple-servers/cmd/api-server/main.go
@@ -1,1 +1,25 @@
 package main
+
+import (
+	"flag"
+	"log"
+	"os"
+	"servers/api"
+
+	"github.com/joho/godotenv"
+)
+
+func main() {
+	// Load environment variables
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatalf("Error loading .env file %v", err)
+	}
+	databaseURL := os.Getenv("DATABASE_URL")
+
+	// Parse command line flags
+	dbURL := flag.String("path", databaseURL, "path where the static files are read from")
+	port := flag.String("port", "8080", "port to run the static server")
+	flag.Parse()
+	api.Run(dbURL, port)
+}
