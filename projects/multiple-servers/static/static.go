6b72730c60c71d79c86de4416ea55dab3d0713bd
SaadiaELF <saadiaelfekak@gmail.com>
2024-04-13 11:57:08 +0000

    Add command line flags

--- projects/multiple-servers/static/static.go
+++ projects/multiple-servers/static/static.go
@@ -1,7 +1,15 @@
 package static
 
-import "fmt"
+import (
+	"flag"
+	"fmt"
+)
 
 func Run() {
+	path := flag.String("path", "./", "path where the static files are read from")
+	port := flag.String("port", "8080", "port to run the static server")
+	flag.Parse()
+	fmt.Println("path:", *path)
+	fmt.Println("port:", *port)
 	fmt.Println("Hello from static server!")
 }
