478e8b874744e3f9ba3ab2213a7db42493e1d12f
SaadiaELF <saadiaelfekak@gmail.com>
2024-04-16 11:59:28 +0100

    Changed default port

--- projects/multiple-servers/cmd/static-server/main.go
+++ projects/multiple-servers/cmd/static-server/main.go
@@ -8,7 +8,7 @@ import (
 func main() {
 	// Parse command line flags
 	path := flag.String("path", "./", "path where the static files are read from")
-	port := flag.String("port", "8080", "port to run the static server")
+	port := flag.String("port", "8082", "port to run the static server")
 	flag.Parse()
 	static.Run(path, port)
 }
