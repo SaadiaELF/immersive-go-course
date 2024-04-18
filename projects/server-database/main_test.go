63878cf28cbf6110846661520f783828e9063275
SaadiaELF <saadiaelfekak@gmail.com>
2024-04-18 15:12:46 +0100

    Refactor test cases and mock setup in main_test.go

--- projects/server-database/main_test.go
+++ projects/server-database/main_test.go
@@ -3,58 +3,67 @@ package main
 import (
 	"net/http"
 	"net/http/httptest"
-	"server-database/types"
+	"strings"
 	"testing"
 
-	"github.com/stretchr/testify/require"
+	"github.com/pashagolub/pgxmock/v3"
 )
 
-func TestFetchImages(t *testing.T) {
-	expected := []types.Image{
-		{
-			Title:   "Sunset",
-			AltText: "Clouds at sunset",
-			URL:     "https://images.unsplash.com/photo-1506815444479-bfdb1e96c566?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1000&q=80",
-		},
-		{
-			Title:   "Mountain",
-			AltText: "A mountain at sunset",
-			URL:     "https://images.unsplash.com/photo-1540979388789-6cee28a1cdc9?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1000&q=80",
-		},
+func TestPostImage(t *testing.T) {
+	mock, err := pgxmock.NewPool()
+	if err != nil {
+		t.Fatal(err)
 	}
-	actual, err := fetchImages()
-	require.NoError(t, err)
-	require.Equal(t, expected, actual)
-}
+	defer mock.Close()
 
-func TestGetIndentParam(t *testing.T) {
-	testCases := []struct {
-		name     string
-		query    string
-		expected string
-	}{
+	type test struct {
+		name               string
+		image              string
+		imageUrl           string
+		expectedStatusCode int
+		expectSelect       bool
+		selectResultRow    *pgxmock.Rows
+		expectInsert       bool
+		// expectInsertValues []
+	}
+
+	testCases := []test{
 		{
-			name:     "case 1:indent=0",
-			query:    "indent=0",
-			expected: "",
+			name:               "empty image",
+			image:              "",
+			imageUrl:           "/image",
+			expectedStatusCode: http.StatusBadRequest,
+			expectSelect:       false,
+			expectInsert:       false,
 		},
 		{
-			name:     "case 2:indent=2",
-			query:    "indent=2",
-			expected: "  ",
-		},
-		{
-			name:     "case 3:indent=4",
-			query:    "indent=4",
-			expected: "    ",
+			name:               "existing image url",
+			image:              `{"title": "image", "url": "/image", "alt_text": "img"}`,
+			imageUrl:           "/image",
+			expectedStatusCode: http.StatusConflict,
+			expectSelect:       true,
+			selectResultRow:    mock.NewRows([]string{"url"}).AddRow("/image"),
+			expectInsert:       false,
 		},
 	}
 
 	for _, tc := range testCases {
-		t.Run(tc.name, func(t *testing.T) {
-			req := httptest.NewRequest(http.MethodGet, "/images.json?"+tc.query, nil)
-			actual := getIndentParam(req)
-			require.Equal(t, tc.expected, actual)
-		})
+
+		if tc.expectSelect {
+			query := `SELECT url FROM public.images WHERE url = $1`
+			mock.ExpectQuery(query).
+				WithArgs(tc.imageUrl).
+				WillReturnRows(tc.selectResultRow)
+		}
+		// if tc.expectInsert {
+		// TODO: Implement the expectInsert branch.
+		// }
+
+		req, _ := http.NewRequest(http.MethodPost, tc.imageUrl, strings.NewReader(tc.image))
+		resp := httptest.NewRecorder()
+		postImage(resp, req)
+		if tc.expectedStatusCode != resp.Code {
+			t.Errorf("testcase %s: got %v, want %v", tc.name, tc.expectedStatusCode, resp.Code)
+		}
 	}
 }
