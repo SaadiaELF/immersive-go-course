152cee597238533dc737a815eebda21399711ae4
SaadiaELF <saadiaelfekak@gmail.com>
2024-04-23 20:27:22 +0100

    Add Ctrl+C handling and process killing functionality

--- mini-projects/cmd/ctrlc.go
+++ mini-projects/cmd/ctrlc.go
@@ -8,7 +8,6 @@ import (
 )
 
 func CtrlC() {
-
 	sigCh := make(chan os.Signal, 1)
 	signal.Notify(sigCh, syscall.SIGINT)
 
@@ -16,5 +15,21 @@ func CtrlC() {
 	fmt.Println("\rCtrl+C is not responding")
 	<-sigCh
 	os.Exit(0)
+}
 
+func CtrlCKill() {
+	var pid int
+
+	fmt.Println("\rEnter the PID of the process you want to kill: ")
+	_, err := fmt.Scan(&pid)
+	if err != nil {
+		fmt.Println("Error: ", err)
+		os.Exit(1)
+	}
+	err = syscall.Kill(pid, syscall.SIGINT)
+	if err != nil {
+		fmt.Println("Error: ", err)
+		os.Exit(1)
+	}
+	fmt.Printf("Process %v killed: ", pid)
 }
--- mini-projects/main.go
+++ mini-projects/main.go
@@ -7,5 +7,5 @@ import (
 
 func main() {
 	fmt.Println("Hello...")
-	cmd.CtrlC()
+	cmd.CtrlCKill()
 }
