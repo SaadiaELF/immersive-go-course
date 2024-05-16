bd298ddfc20c6d3cc22d70849a6e09a30c577586
SaadiaELF <saadiaelfekak@gmail.com>
2024-05-16 18:17:13 +0100

    chore: Add e2e test for routes

---
+++ projects/docker-cloud/e2e/main_test.go
@@ -0,0 +1,54 @@
+package e2e_test
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"testing"
+)
+
+func TestRoutes(t *testing.T) {
+	testcases := []struct {
+		name     string
+		endpoint string
+		expected string
+	}{
+		{
+			name:     "root",
+			endpoint: "/",
+			expected: "Hello, world!\n",
+		},
+		{
+			name:     "ping",
+			endpoint: "/ping",
+			expected: "pong\n",
+		},
+	}
+	for _, tc := range testcases {
+		t.Run(tc.name, func(t *testing.T) {
+			request, err := http.NewRequest("GET", fmt.Sprintf("http://localhost:80%s", tc.endpoint), nil)
+			if err != nil {
+				t.Fatalf("Could not create request: %v", err)
+			}
+
+			response, err := http.DefaultClient.Do(request)
+			if err != nil {
+				t.Fatalf("Could not make request: %v", err)
+			}
+
+			if response.StatusCode != http.StatusOK {
+				t.Errorf("Expected status 200, got %d", response.StatusCode)
+			}
+			body, err := io.ReadAll(response.Body)
+			if err != nil {
+				t.Fatalf("Could not read response body: %v", err)
+			}
+			if string(body) != tc.expected {
+				t.Errorf("Expected %q, got %q", tc.expected, string(body))
+			}
+
+			defer response.Body.Close()
+		})
+	}
+
+}
