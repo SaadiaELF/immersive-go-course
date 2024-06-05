1260eb02bb21f1c7f9951ef8c4df86af828d98d8
SaadiaELF <saadiaelfekak@gmail.com>
2024-06-05 09:44:57 +0100

    feat: Schedule and execute cron jobs

--- projects/kafka-cron/cmd/producer/producer.go
+++ projects/kafka-cron/cmd/producer/producer.go
@@ -2,10 +2,15 @@ package main
 
 import (
 	"encoding/json"
+	"fmt"
 	"io"
 	"kafka-cron/cmd/producer/pkg/models"
 	"log"
 	"os"
+	"os/exec"
+	"time"
+
+	"github.com/robfig/cron/v3"
 )
 
 // Read and parse the configuration file
@@ -40,15 +45,52 @@ func parseConfig(data []byte) ([]models.CronJob, error) {
 
 }
 
+func scheduleJobs(jobs []models.CronJob) {
+	c := cron.New(cron.WithSeconds())
+
+	// Schedule the jobs
+	for _, job := range jobs {
+		_, err := c.AddFunc(job.Schedule, func() {
+			err := executeJob(job)
+			if err != nil {
+				fmt.Println("error executing job:", job.Name)
+			}
+		})
+		if err != nil {
+			fmt.Println("error scheduling job:", job.Name)
+		}
+	}
+
+	// Start the Cron job scheduler
+	c.Start()
+
+	// Wait for the Cron job to run
+	time.Sleep(5 * time.Minute)
+
+	// Stop the Cron job scheduler
+	c.Stop()
+
+}
+
+func executeJob(job models.CronJob) error {
+	cmd := exec.Command("sh", "-c", job.Command)
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	fmt.Println("Executing job:", job.Name)
+	return cmd.Run()
+}
+
 func main() {
 	data, err := readConfig()
 	if err != nil {
 		log.Fatalf("error reading the configuration file: %v", err)
 	}
 
-	_, err = parseConfig(data)
+	jobs, err := parseConfig(data)
 	if err != nil {
 		log.Fatalf("error parsing the configuration file: %v", err)
 	}
+
+	scheduleJobs(jobs)
 
 }
