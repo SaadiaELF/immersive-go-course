948cd5361de83c8193fce24ef776c00c135c4aba
SaadiaELF <saadiaelfekak@gmail.com>
2024-06-05 12:54:45 +0100

    Refactor Scheduler function to accept topic as a parameter

--- projects/kafka-cron/producer/internal/scheduler/scheduler.go
+++ projects/kafka-cron/producer/internal/scheduler/scheduler.go
@@ -3,20 +3,20 @@ package scheduler
 import (
 	"fmt"
 	"kafka-cron/producer/internal/producer"
-	"kafka-cron/producer/pkg/models"
+	"kafka-cron/pkg/models"
 	"time"
 
 	"github.com/confluentinc/confluent-kafka-go/kafka"
 	"github.com/robfig/cron/v3"
 )
 
-func Scheduler(p *kafka.Producer, jobs []models.CronJob) error {
+func Scheduler(p *kafka.Producer, jobs []models.CronJob, topic string) error {
 	c := cron.New(cron.WithSeconds())
 
 	// Schedule the jobs
 	for _, job := range jobs {
 		_, err := c.AddFunc(job.Schedule, func() {
-			err := producer.ProduceMessage(p, "cron-topic", job)
+			err := producer.ProduceMessage(p, topic, job)
 			if err != nil {
 				fmt.Printf("error producing message: %v", err)
 			}
