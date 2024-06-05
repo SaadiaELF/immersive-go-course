4ef707a0921dd6b172840b21be427a0141744191
SaadiaELF <saadiaelfekak@gmail.com>
2024-06-05 11:58:13 +0100

    Refactor message struct and remove unused code

--- projects/kafka-cron/producer/pkg/models/message.go
+++
@@ -1,9 +0,0 @@
-package models
-
-import "github.com/google/uuid"
-
-type Message struct {
-	Id       uuid.UUID `json:"id"`
-	Command  string    `json:"command"`
-	Schedule string    `json:"scheduler"`
-}
