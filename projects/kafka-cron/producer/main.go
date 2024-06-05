efb248d9002db698c691d5e3a7e88436a632c041
SaadiaELF <saadiaelfekak@gmail.com>
2024-06-05 11:18:37 +0100

    feat: Create Kafka producer and topic for cron jobs

--- projects/kafka-cron/producer/main.go
+++ projects/kafka-cron/producer/main.go
@@ -1,12 +1,15 @@
 package main
 
 import (
+	"fmt"
+	"kafka-cron/producer/internal/producer"
 	"kafka-cron/producer/internal/scheduler"
 	"kafka-cron/producer/internal/utils"
 	"log"
 )
 
 func main() {
+	// Read and parse the configuration file
 	data, err := utils.ReadConfig()
 	if err != nil {
 		log.Fatalf("error reading the configuration file: %v", err)
@@ -17,6 +20,17 @@ func main() {
 		log.Fatalf("error parsing the configuration file: %v", err)
 	}
 
-	scheduler.Scheduler(jobs)
+	// Create the Kafka producer
+	p, err := producer.Producer()
+	if err != nil {
+		fmt.Printf("Error creating Kafka producer: %v", err)
+	}
+
+	// Create the Kafka topic
+	err = producer.CreateTopic(p, "cron-topic")
+	if err != nil {
+		fmt.Printf("Error creating Kafka topic: %v", err)
+	}
+	scheduler.Scheduler(p, jobs)
 
 }
