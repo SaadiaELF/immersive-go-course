a6d908ceb4d888ef2b2f0a8d49d48816474680e0
SaadiaELF <saadiaelfekak@gmail.com>
2024-06-05 12:53:56 +0100

    feat: Add JSON unmarshaling for cron job messages in Kafka consumer

--- projects/kafka-cron/consumer/main.go
+++ projects/kafka-cron/consumer/main.go
@@ -1,20 +1,24 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"kafka-cron/consumer/executor"
 	"kafka-cron/consumer/internal/consumer"
+	"kafka-cron/pkg/models"
+	"kafka-cron/utils"
 	"os"
 )
 
 func main() {
-
-	consumer, err := consumer.Consumer()
+	topic, brokers := utils.Args()
+	consumer, err := consumer.Consumer(brokers)
 	if err != nil {
 		fmt.Printf("failed to create consumer: %s\n", err)
 		os.Exit(1)
 	}
 
-	err = consumer.Subscribe("cron-topic", nil)
+	err = consumer.Subscribe(topic, nil)
 	if err != nil {
 		fmt.Printf("failed to subscribe to topic: %s\n", err)
 		os.Exit(1)
@@ -24,6 +28,13 @@ func main() {
 		msg, err := consumer.ReadMessage(-1)
 		if err == nil {
 			fmt.Printf("Message on %s: %s\n", msg.TopicPartition, string(msg.Value))
+			var job models.CronJob
+			err = json.Unmarshal(msg.Value, &job)
+			if err != nil {
+				fmt.Printf("failed to unmarshal message: %s\n", err)
+				continue
+			}
+			executor.Executor(job)
 		} else {
 			fmt.Printf("Consumer error: %v (%v)\n", err, msg)
 		}
