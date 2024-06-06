b67ccf9ca2498d483f507eb6607e245f544506e8
SaadiaELF <saadiaelfekak@gmail.com>
2024-06-06 16:59:17 +0100

    refactor: Add cluster parameter to Kafka producer arguments in utils.Args()

--- projects/kafka-cron/utils/args.go
+++ projects/kafka-cron/utils/args.go
@@ -2,12 +2,14 @@ package utils
 
 import "flag"
 
-func Args() (string, string, string) {
+func Args() (string, string, string, string) {
 	topic1 := flag.String("topic1", "topic-cluster-a", "Kafka topic to send messages to")
 	topic2 := flag.String("topic2", "topic-cluster-b", "Kafka topic to send messages to")
 	brokers := flag.String("brokers", "localhost:9092", "Kafka brokers to connect to")
+	cluster := flag.String("cluster", "cluster-a", "Specify the cluster (cluster-a or cluster-b)")
+
 	flag.Parse()
 
-	return *topic1, *topic2, *brokers
+	return *topic1, *topic2, *brokers, *cluster
 
 }
