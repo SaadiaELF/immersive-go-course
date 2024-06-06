aed32eb7167b168634d4a8bd1212683a5c43824f
SaadiaELF <saadiaelfekak@gmail.com>
2024-06-06 11:29:22 +0100

    feat: Update Kafka producer to create multiple topics and schedule jobs for different clusters

--- projects/kafka-cron/utils/args.go
+++ projects/kafka-cron/utils/args.go
@@ -2,11 +2,12 @@ package utils
 
 import "flag"
 
-func Args() (string, string) {
-	topic := flag.String("topic", "cron-jobs", "Kafka topic to send messages to")
+func Args() (string, string, string) {
+	topic1 := flag.String("topic1", "topic-cluster-a", "Kafka topic to send messages to")
+	topic2 := flag.String("topic2", "topic-cluster-b", "Kafka topic to send messages to")
 	brokers := flag.String("brokers", "localhost:9092", "Kafka brokers to connect to")
 	flag.Parse()
 
-	return *topic, *brokers
+	return *topic1, *topic2, *brokers
 
 }
