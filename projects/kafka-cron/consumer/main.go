e1f78b215656ba05b71b6e9ae9b2391a5e2d46e2
SaadiaELF <saadiaelfekak@gmail.com>
2024-06-06 11:53:35 +0100

    refactor: Initialize multiple Kafka consumers for different topics

--- projects/kafka-cron/consumer/main.go
+++ projects/kafka-cron/consumer/main.go
@@ -7,27 +7,57 @@ import (
 	"kafka-cron/consumer/internal/executor"
 	"kafka-cron/pkg/models"
 	"kafka-cron/utils"
-	"os"
+	"sync"
+
+	"github.com/confluentinc/confluent-kafka-go/kafka"
 )
 
 func main() {
-	topic, brokers := utils.Args()
-	consumer, err := consumer.Consumer(brokers)
+	topic1, topic2, brokers := utils.Args()
+	consumer1, err := initializeConsumer(brokers, topic1)
 	if err != nil {
-		fmt.Printf("failed to create consumer: %s\n", err)
-		os.Exit(1)
+		fmt.Printf("failed to initialise consumer: %s\n", err)
+	}
+	consumer2, err := initializeConsumer(brokers, topic2)
+	if err != nil {
+		fmt.Printf("failed to initialise consumer: %s\n", err)
 	}
 
-	err = consumer.Subscribe(topic, nil)
+	var wg sync.WaitGroup
+	wg.Add(2)
+
+	go func() {
+		defer wg.Done()
+		processMessages(consumer1)
+	}()
+
+	go func() {
+		defer wg.Done()
+		processMessages(consumer2)
+	}()
+	wg.Wait()
+}
+
+func initializeConsumer(brokers string, topic string) (*kafka.Consumer, error) {
+	cons, err := consumer.Consumer(brokers)
 	if err != nil {
-		fmt.Printf("failed to subscribe to topic: %s\n", err)
-		os.Exit(1)
+		return nil, fmt.Errorf("failed to create consumer: %s", err)
 	}
 
+	err = cons.Subscribe(topic, nil)
+	if err != nil {
+		return nil, fmt.Errorf("failed to subscribe to topic: %s", err)
+
+	}
+
+	return cons, nil
+}
+
+func processMessages(con *kafka.Consumer) {
 	for {
-		msg, err := consumer.ReadMessage(-1)
+		msg, err := con.ReadMessage(-1)
 		if err == nil {
-			fmt.Printf("Message on %s: ", msg.TopicPartition)
+			fmt.Printf("Message for %s on %s: ", *msg.TopicPartition.Topic, msg.TopicPartition)
 			var job models.CronJob
 			err = json.Unmarshal(msg.Value, &job)
 			if err != nil {
@@ -39,5 +69,4 @@ func main() {
 			fmt.Printf("Consumer error: %v (%v)\n", err, msg)
 		}
 	}
-
 }
