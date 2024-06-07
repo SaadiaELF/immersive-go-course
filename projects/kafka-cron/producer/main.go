dfaefecdf0794900ac1d807456e14240a2cc02bc
SaadiaELF <saadiaelfekak@gmail.com>
2024-06-07 13:47:56 +0100

    refactor: Update Kafka producer

--- projects/kafka-cron/producer/main.go
+++ projects/kafka-cron/producer/main.go
@@ -1,19 +1,24 @@
 package main
 
 import (
-	"kafka-cron/pkg/models"
 	"kafka-cron/producer/internal/producer"
 	"kafka-cron/producer/internal/scheduler"
 	"kafka-cron/producer/utils"
 	"log"
-	"sync"
 	"time"
 )
 
 func main() {
-	topic1, topic2, brokers, cluster := utils.Args()
 
-	jobs, err := utils.ParseConfig(cluster)
+	topic1, topic2, brokers, _ := utils.Args()
+
+	// Read and parse the configuration file
+	data, err := utils.ReadConfig()
+	if err != nil {
+		log.Fatalf("error reading the configuration file: %v", err)
+	}
+
+	jobs, err := utils.ParseConfig(data)
 	if err != nil {
 		log.Fatalf("error parsing the configuration file: %v", err)
 	}
@@ -35,43 +40,16 @@ func main() {
 		log.Printf("Error creating Kafka topic2: %v", err)
 	}
 
-	// Schedule the jobs
-	clusterAJobs := []models.CronJob{}
-	clusterBJobs := []models.CronJob{}
-
-	for _, job := range jobs {
-		switch job.Cluster {
-		case "cluster-a":
-			clusterAJobs = append(clusterAJobs, job)
-		case "cluster-b":
-			clusterBJobs = append(clusterBJobs, job)
-		default:
-			log.Printf("Warning: Jobs has an unknown cluster %s", job.Cluster)
-		}
-	}
-
-	var wg sync.WaitGroup
-	wg.Add(2)
-
+	done := make(chan bool)
 	go func() {
-		defer wg.Done()
-		err := scheduler.Scheduler(p, clusterAJobs, topic1, 5*time.Minute)
+
+		err := scheduler.Scheduler(p, jobs, topic1, topic2, 5*time.Minute)
 		if err != nil {
 			log.Printf("Error scheduling jobs for cluster a: %v", err)
 		}
 	}()
-
-	go func() {
-		defer wg.Done()
-		err = scheduler.Scheduler(p, clusterBJobs, topic2, 5*time.Minute)
-		if err != nil {
-			log.Printf("Error scheduling jobs for cluster b:  %v", err)
-		}
-	}()
-
-	wg.Wait()
+	done <- true
 
 	// Close the producer
 	p.Close()
-
 }
