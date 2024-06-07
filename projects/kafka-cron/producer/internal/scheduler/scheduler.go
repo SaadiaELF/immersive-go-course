73d359133ab154a9f1f2e885d9943695a977685f
SaadiaELF <saadiaelfekak@gmail.com>
2024-06-07 13:48:28 +0100

    refactor: Update Scheduler to handle multiple Kafka topics based on job clusters

--- projects/kafka-cron/producer/internal/scheduler/scheduler.go
+++ projects/kafka-cron/producer/internal/scheduler/scheduler.go
@@ -4,17 +4,28 @@ import (
 	"fmt"
 	"kafka-cron/pkg/models"
 	"kafka-cron/producer/internal/producer"
+	"log"
 	"time"
 
 	"github.com/confluentinc/confluent-kafka-go/kafka"
 	"github.com/robfig/cron/v3"
 )
 
-func Scheduler(p *kafka.Producer, jobs []models.CronJob, topic string, duration time.Duration) error {
+func Scheduler(p *kafka.Producer, jobs []models.CronJob, topic1 string, topic2 string, duration time.Duration) error {
+
 	c := cron.New(cron.WithSeconds())
 
 	// Schedule the jobs
 	for _, job := range jobs {
+		var topic string
+		switch job.Cluster {
+		case "cluster-a":
+			topic = topic1
+		case "cluster-b":
+			topic = topic2
+		default:
+			log.Printf("Warning: Jobs has an unknown cluster %s", job.Cluster)
+		}
 		_, err := c.AddFunc(job.Schedule, func() {
 			err := producer.ProduceMessage(p, topic, job)
 			if err != nil {
