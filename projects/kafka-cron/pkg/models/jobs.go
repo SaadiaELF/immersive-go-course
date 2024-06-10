db0b253593bcbc5d6d5ea5edea07bdb00c39ada0
SaadiaELF <saadiaelfekak@gmail.com>
2024-06-10 10:00:27 +0100

    refactor: Update CronJob struct with additional fields for retries and retry interval

--- projects/kafka-cron/pkg/models/jobs.go
+++ projects/kafka-cron/pkg/models/jobs.go
@@ -1,7 +1,12 @@
 package models
 
 type CronJob struct {
-	Schedule string `json:"schedule"`
-	Command  string `json:"command"`
-	Cluster  string `json:"cluster"`
+	Name          string `json:"name"`
+	Id            string `json:"id"`
+	Schedule      string `json:"schedule"`
+	Command       string `json:"command"`
+	Cluster       string `json:"cluster"`
+	Retries       int    `json:"retries"`
+	RetryTopic    string `json:"retry_topic"`
+	RetryInterval int    `json:"retry_interval"`
 }
