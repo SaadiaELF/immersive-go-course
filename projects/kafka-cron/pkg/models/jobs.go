823e0b796fe80e56efe08e04d3da1b2589cc90f1
SaadiaELF <saadiaelfekak@gmail.com>
2024-06-11 20:16:04 +0100

    refactor: Add start time and latency fields to CronJob model

--- projects/kafka-cron/pkg/models/jobs.go
+++ projects/kafka-cron/pkg/models/jobs.go
@@ -1,11 +1,15 @@
 package models
 
+import "time"
+
 type CronJob struct {
-	Id            string `json:"id"`
-	Schedule      string `json:"schedule"`
-	Command       string `json:"command"`
-	Cluster       string `json:"cluster"`
-	Retries       int    `json:"retries"`
-	RetryTopic    string `json:"retry_topic"`
-	RetryInterval int    `json:"retry_interval"`
+	Id            string        `json:"id"`
+	Schedule      string        `json:"schedule"`
+	Command       string        `json:"command"`
+	Cluster       string        `json:"cluster"`
+	Retries       int           `json:"retries"`
+	RetryTopic    string        `json:"retry_topic"`
+	RetryInterval int           `json:"retry_interval"`
+	StartTime     time.Time     `json:"start_time"`
+	Latency       time.Duration `json:"latency"`
 }
