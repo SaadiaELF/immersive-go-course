eb8deef939daec4b8cd8a493b3630989c3ebd994
SaadiaELF <saadiaelfekak@gmail.com>
2024-06-11 20:15:54 +0100

    refactor: Improve job execution logging and latency measurement

--- projects/kafka-cron/consumer/internal/executor/exectutor.go
+++ projects/kafka-cron/consumer/internal/executor/exectutor.go
@@ -5,12 +5,23 @@ import (
 	"kafka-cron/pkg/models"
 	"os"
 	"os/exec"
+	"time"
 )
 
 func Execute(job models.CronJob) error {
 	cmd := exec.Command("sh", "-c", job.Command)
+	job.Latency = time.Since(job.StartTime)
+
+	models.CronJobLatency.Observe(float64(job.Latency.Microseconds()))
+
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
-	fmt.Println("Executing job:", job.Id)
-	return cmd.Run()
+
+	fmt.Printf("Executing job:%s\n", job.Id)
+	err := cmd.Run()
+	if err != nil {
+		return fmt.Errorf("failed to execute job: %w", err)
+	}
+	fmt.Printf("Job executed successfully with latency: %s\n", job.Latency)
+	return nil
 }
