a742182ee9ce412c16cfad822c5411d2637fb853
SaadiaELF <saadiaelfekak@gmail.com>
2024-06-12 19:39:10 +0100

    refactor: Update CronJob model to measure job latency in milliseconds

--- projects/kafka-cron/pkg/models/metrics.go
+++ projects/kafka-cron/pkg/models/metrics.go
@@ -3,9 +3,9 @@ package models
 import "github.com/prometheus/client_golang/prometheus"
 
 var (
-	CronJobLatency = prometheus.NewHistogram(prometheus.HistogramOpts{
+	CronJobLatency = prometheus.NewHistogramVec(prometheus.HistogramOpts{
 		Name:    "cron_job_latency_milliseconds",
 		Help:    "Latency of cron job execution in milliseconds.",
 		Buckets: prometheus.LinearBuckets(0, 5, 20),
-	})
+	}, []string{"cluster"})
 )
