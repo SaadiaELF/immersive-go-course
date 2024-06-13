e519e55ce8ac9fe0f60b6f6a5ee899d8b655cf28
SaadiaELF <saadiaelfekak@gmail.com>
2024-06-13 14:09:17 +0100

    refactor: Add metrics for cron job count and error count in kafka-cron consumer

--- projects/kafka-cron/pkg/models/metrics.go
+++ projects/kafka-cron/pkg/models/metrics.go
@@ -9,3 +9,17 @@ var (
 		Buckets: prometheus.LinearBuckets(0, 10, 10),
 	}, []string{"cluster"})
 )
+
+var (
+	CronJobCount = prometheus.NewCounterVec(prometheus.CounterOpts{
+		Name: "cron_job_count",
+		Help: "Number of cron jobs executed.",
+	}, []string{"cluster"})
+)
+
+var (
+	CronJobErrorCount = prometheus.NewCounterVec(prometheus.CounterOpts{
+		Name: "cron_job_error_count",
+		Help: "Number of cron jobs that failed.",
+	}, []string{"cluster"})
+)
