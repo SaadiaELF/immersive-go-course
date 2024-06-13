115434c7dc4da7c0a0a2ae35966bc684377ee646
SaadiaELF <saadiaelfekak@gmail.com>
2024-06-13 13:31:26 +0100

    refactor: Add random failure for job execution in kafka-cron consumer

--- projects/kafka-cron/consumer/internal/executor/exectutor.go
+++ projects/kafka-cron/consumer/internal/executor/exectutor.go
@@ -3,12 +3,15 @@ package executor
 import (
 	"fmt"
 	"kafka-cron/pkg/models"
+	"math/rand"
 	"os"
 	"os/exec"
 	"time"
 )
 
 func Execute(job models.CronJob) error {
+	n := rand.Intn(3)
+
 	cmd := exec.Command("sh", "-c", job.Command)
 	job.Latency = time.Since(job.StartTime)
 
@@ -18,10 +21,13 @@ func Execute(job models.CronJob) error {
 	cmd.Stderr = os.Stderr
 
 	fmt.Printf("Executing job:%s\n", job.Id)
+	if n == 2 || n == 1{
+		return fmt.Errorf("failed to execute job")
+	}
 	err := cmd.Run()
 	if err != nil {
 		return fmt.Errorf("failed to execute job: %w", err)
 	}
-	fmt.Printf("Job executed successfully with latency: %s\n", job.Latency)
+	// fmt.Printf("Job executed successfully with latency: %s\n", job.Latency)
 	return nil
 }
