8d82d365d71c87cc6cbbb67bb624decf6883b5b9
SaadiaELF <saadiaelfekak@gmail.com>
2024-06-13 14:09:11 +0100

    refactor: Update job execution in kafka-cron consumer to include latency observation and job count increment

--- projects/kafka-cron/consumer/internal/executor/exectutor.go
+++ projects/kafka-cron/consumer/internal/executor/exectutor.go
@@ -16,18 +16,19 @@ func Execute(job models.CronJob) error {
 	job.Latency = time.Since(job.StartTime)
 
 	models.CronJobLatency.WithLabelValues(job.Cluster).Observe(float64(job.Latency.Milliseconds()))
+	models.CronJobCount.WithLabelValues(job.Cluster).Inc()
 
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
 
 	fmt.Printf("Executing job:%s\n", job.Id)
-	if n == 2 || n == 1{
+	if n == 2 || n == 1 {
 		return fmt.Errorf("failed to execute job")
 	}
 	err := cmd.Run()
 	if err != nil {
 		return fmt.Errorf("failed to execute job: %w", err)
 	}
-	// fmt.Printf("Job executed successfully with latency: %s\n", job.Latency)
+
 	return nil
 }
